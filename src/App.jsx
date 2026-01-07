75f565969cf01f235016ca4db9267a29664f537e
ian-michael254 <ian.michael.codes@gmail.com>
2026-01-07 21:06:18 +0300

    Project Setup with vite both tailwindcss and react js

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-2 text-red-500 bg-slate-300 underline text-5xl'>
+      App
+    </div>
+  )
+}
+
+export default App
